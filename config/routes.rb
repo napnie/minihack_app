b10793e15047e42cc2854e003f7348a0bf3b92f4
Nitith Chayakul <nitith.c@ku.th>
2017-04-24 08:59:42 +0000

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
   
-  get 'static_pages/dom'
+  get 'static_pages/dom', :controller => "dom"
   
-  get 'static_pages/ac2'
+  get 'static_pages/ac2', :controller => "ac2"
   
-  get 'static_pages/ac1'
+  get 'static_pages/ac1', :controller => "ac1"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
